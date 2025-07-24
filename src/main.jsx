fe894d1ab2e6d84f580f0b208f25d2d41a8ff009
Bogdan Vulea <bogdanvulea@gmail.com>
2025-07-24 11:40:54 +0300

    Program Tabara CT

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import TabaraCT from './TabaraCT'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <TabaraCT />
+  </React.StrictMode>,
+)
